6aa29997fe796fe561fbc43be7b18997584f2529
Serdar Hasan <63472762+shasan033@users.noreply.github.com>
2023-12-18 22:07:18 -0800

    Add project files.

---
+++ BluetoothClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+
+namespace DynaLightBlue
+{
+    internal class BluetoothClient
+    {
+
+        public BluetoothClient(string uuid, string mac) { }
+
+        public void write_characteristic() { }
+
+        public void turn_on() { }
+
+        public void turn_off() { }
+
+        public void set_rgb() { }
+
+        public void connect() { }
+    }
+}
